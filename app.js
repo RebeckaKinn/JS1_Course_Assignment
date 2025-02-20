3413ccdd9e33129703284919c7f0b8655f7ae253
RebeckaKinn <rebeckaki@getacademy.no>
2025-02-20 12:58:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ import { model } from "./model.js";
 updateView();
 
 function updateView(){
-    renderPage("home");
+    model.app.display.innerHTML = /*HTML*/ `
+    <header>${navbar()}</header>
+    <main>${renderPage("home")}</main>
+    <footer></footer>
+ 
+    `;
+    
 }
 
 
 function renderPage(pageName) {
     const page = model.app.pages.find(p => p.name === pageName);
     if (page) {
-        model.app.display.innerHTML = page.path; 
+        return page.path; 
     }
 }
 
-
+function navbar(){
+    return /*HTML*/ `
+    <ul>
+        <li>Option 1</li>
+        <li>Option 2</li>
+        <li>Option 3</li>
+    </ul>
+    
+    `;
+}
