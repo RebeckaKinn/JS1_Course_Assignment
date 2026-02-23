9e34645fdc8da9628f2e6567d90e909bbf08cc98
Rebecka Kinn <rebeckaki@getacademy.no>
2026-02-23 15:00:17 +0100

    changeing item display and general css

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,6 +1,11 @@
 import { model } from "../../model.js";
 import { fetchMainData } from "../../api.js";
-import { chosenProduct, handleAddToCart, filterController, resetFilter } from "../../controller.js";
+import {
+  chosenProduct,
+  handleAddToCart,
+  filterController,
+  resetFilter,
+} from "../../controller.js";
 import { errorMessage } from "../../components/errorMessage/errorMessage.js";
 
 window.chosenProduct = chosenProduct;
@@ -9,46 +14,45 @@ window.filterController = filterController;
 window.resetFilter = resetFilter;
 window.errorMessage = errorMessage;
 
-export async function homeView(){
-    const items = await displayItems();
-    if(items === null) return errorMessage();
-    return /*HTML*/`
+export async function homeView() {
+  const items = await displayItems();
+  if (items === null) return errorMessage();
+  return /*HTML*/ `
     <section class="flex col">
     <h1>Welcome to PlayBox </h1>
-    <h2>– Your Ultimate Gaming Destination!</h2>
+    <h2>Your Ultimate Gaming Destination!</h2>
     <p>Discover the latest and greatest games right here at PlayBox! Whether you're into action-packed adventures, thrilling RPGs, or competitive multiplayer battles, we have something for every gamer. Explore top-rated titles, exclusive deals, and must-play classics—all in one place.</p>
     <ul>
         <li>
-            <p>🔥 New Releases & Bestsellers</p>
+            <p>&#10022; New Releases & Bestsellers</p>
         </li>
         <li>
-            <p>🎮 Exclusive Discounts & Special Offers</p>
+            <p>&#10022; Exclusive Discounts & Special Offers</p>
         </li>
         <li>
-            <p>🌍 Games for Every Platform & Genre</p>
+            <p>&#10022; Games for Every Platform & Genre</p>
         </li>
     </ul>
     <p>Find your next favorite game below and start playing today! 🚀👇</p>
     <section class="filter-container">${filterButton()}</section>
     
-    <div>${items}</div>
+    <div class="item-container">${items}</div>
     </section>
     
     
     `;
 }
 
-
-async function displayItems(){
-    try{
-        const data = await fetchMainData();
-        const list = data || [];
-        if (list.length === 0) {
-            return null; 
-        }
-        let items = '';
-        list.forEach(element => {
-            items += /*HTML*/`
+async function displayItems() {
+  try {
+    const data = await fetchMainData();
+    const list = data || [];
+    if (list.length === 0) {
+      return null;
+    }
+    let items = "";
+    list.forEach((element) => {
+      items += /*HTML*/ `
             <section class="product-item">
 
                 <div onclick="chosenProduct('${element.id}')" class="list-image">
@@ -64,16 +68,20 @@ async function displayItems(){
                     </div>
                     
                     
-                    <div class="flex row align-items-baseline gap-5 remove-margin">
-                        <p>Price:</p>
-                        ${element.onSale ? `
-                            <p class="discount">$${element.price}</p> 
-                            <p class="red price">$${element.discountedPrice}</p>
-                            `: `<p class="price">$${element.price}</p>`}
+                    <div class="flex row align-items-baseline gap-5">
+                        <p class="remove-margin">Price:</p>
+                        ${
+                          element.onSale
+                            ? `
+                            <p class="discount remove-margin">$${element.price}</p> 
+                            <p class="red price remove-margin">$${element.discountedPrice}</p>
+                            `
+                            : `<p class="price remove-margin">$${element.price}</p>`
+                        }
                     </div>
                         
 
-                    <div class="height-100 flex col gap-10 justify-content-center">
+                    <div class=" flex gap-1rem  ">
                         <button class="square square2" onclick="chosenProduct('${element.id}')">View</button>
                         <button class="square scale-09" onclick="handleAddToCart(this, '${element.id}', '${element.title}', ${element.price}, ${element.discountedPrice}, ${element.onSale}, '${element.image.url}', '${element.image.alt}')">Add to Cart</button>
                     </div>
@@ -84,37 +92,34 @@ async function displayItems(){
 
             </section>
             `;
-        });
-        
-        return items;
-    } catch (error){
-        console.error(error.message);
-        return null;
-    }
+    });
+
+    return items;
+  } catch (error) {
+    console.error(error.message);
+    return null;
+  }
 }
 
 function filterButton() {
- return /*HTML*/`
+  return /*HTML*/ `
     <fieldset class="dropdownFilter">
         <legend class="filterButton" >Filter by <b>${model.input.currentFilter == "" ? "category" : model.input.currentFilter}</b></legend>
         <ul class="filterOptions" id="filterOptions">
-            ${model.data.filter.genre.map(genre => /*HTML*/`
+            ${model.data.filter.genre
+              .map(
+                (genre) => /*HTML*/ `
                 <li>
                     <input type="radio" name="filter_options" value="${genre}" id="${genre}" onclick="filterController(this.value)" ${model.input.currentFilter === genre ? "checked" : ""}>
                     <label for="${genre}">${genre}</label>
                 </li>
-            `).join("")}
+            `,
+              )
+              .join("")}
             <li>
                 <p onclick="resetFilter()">Reset</p>
             </li>
         </ul>
     </fieldset>
     `;
-
 }
-
-
-
-
-
-
